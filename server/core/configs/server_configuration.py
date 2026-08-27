19eba31a4a81c147877c32d76291ac96483c6c72
Cisco248 <lahirud608@gmail.com>
2026-08-27 21:55:01 +0530

    medel uploads

--- server/core/configs/server_configuration.py
+++ server/core/configs/server_configuration.py
@@ -40,24 +40,24 @@ class ServerSettings(BaseSettings):
     )
 
     # Base Models Paths Configurations
-    HYPERTENSION_PATH: str = "gs://server-models/base/hypertension"
+    HYPERTENSION_PATH: str = "/app/data/base/hypertension"
     HYPERTENSION_MODEL_PATH: str = f"{HYPERTENSION_PATH}/risk_classifier.pkl"
     HYPERTENSION_FEATURE_PATH: str = f"{HYPERTENSION_PATH}/feature_names.pkl"
     HYPERTENSION_LABEL_PATH: str = f"{HYPERTENSION_PATH}/risk_labels.pkl"
 
-    DIABETES_PATH: str = "gs://server-models/base/diabetes"
+    DIABETES_PATH: str = "/app/data/base/diabetes"
     DIABETES_MODEL_PATH: str = f"{DIABETES_PATH}/model.pkl"
     DIABETES_SCALER_PATH: str = f"{DIABETES_PATH}/scaler.pkl"
     DIABETES_FEATURE_PATH: str = f"{DIABETES_PATH}/features.pkl"
 
-    HAR_PATH: str = "gs://server-models/har"
-    HAR_MODEL_PATH: str = f"{HAR_PATH}/model.pkl"
-
-    HEART_DISEASE_PATH: str = "gs://server-models/base/heart_disease"
+    HEART_DISEASE_PATH: str = "/app/data/base/heart_disease"
     HEART_DISEASE_MODEL_PATH: str = f"{HEART_DISEASE_PATH}/model.pkl"
     HEART_DISEASE_SCALER_PATH: str = f"{HEART_DISEASE_PATH}/scaler.pkl"
     HEART_DISEASE_FEATURE_PATH: str = f"{HEART_DISEASE_PATH}/features.json"
     HEART_DISEASE_INFO_PATH: str = f"{HEART_DISEASE_PATH}/model.json"
+
+    HAR_PATH: str = "/app/data/har"
+    HAR_MODEL_PATH: str = f"{HAR_PATH}/model.pkl"
 
     @property
     def rag_url(self) -> str:
